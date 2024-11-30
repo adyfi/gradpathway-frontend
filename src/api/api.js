2edd3578809fc7c3265b1d750c45e210030c50f1
saurabhdhakne <sbdhakne309@gmail.com>
2024-11-30 10:52:50 +0530

    API file integration

---
+++ src/api/api.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+export const login = async (email, password) => {
+  const email = process.env.REACT_APP_USER_EMAIL;
+  const password = process.env.REACT_APP_USER_PASSWORD;
+  const baseURL = process.env.BACKEND_BASE_URL;
+
+  const data = JSON.stringify({ email, password });
+  const config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: `${baseURL}/users/login/`,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    data: data
+  };
+
+  try {
+    const response = await axios.request(config);
+    console.log(JSON.stringify(response.data));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const fetchBlogPosts = async (AUTH_TOKEN) => {
+  const config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: `${BASE_URL}/blogs/`,
+    headers: { 'Content-Type': 'application/json', 'Authorization': AUTH_TOKEN }
+  };
+  try {
+    const response = await axios.request(config);
+    console.log(JSON.stringify(response.data));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const fetchBlogPostById = async (AUTH_TOKEN, blogId) => {
+  const config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: `${BASE_URL}/blogs/${blogId}`,
+    headers: { 'Authorization': AUTH_TOKEN },
+  };
+  try { 
+    const response = await axios.request(config); 
+    console.log(JSON.stringify(response.data)); 
+  } catch (error) {
+    console.error(error); 
+  }
+};
