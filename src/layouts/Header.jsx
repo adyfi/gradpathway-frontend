ddd0233c86d71f6e99725fc81f98c88368ab072b
saurabhdhakne <sbdhakne309@gmail.com>
2024-12-19 00:11:43 +0530

    navbar updated

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
-const Header = () => <header>
-  {/* <!-- main menu section HTML  --> */}
-  <h1 className='bg-danger'>
-    Protected Header
-  </h1>
-  {/* <!-- main menu section HTML END --> */}
-</header>;
+const Header = () => {
+  return (
+    <>
+    <div className="contaioner-fluid">
+      <div className="row">
+        <div className="col-md-2 bg-primary" style={{height:'100vh'}}>
+
+        </div>
+        <div className="col-md-10 bg-danger p-0">
+          <div className="navbar col-12 bg-dark pt-5">
+
+          </div>
+        </div>
+      </div>
+    </div>
+   
+    </>
+
+  );
+}
 export default Header;
