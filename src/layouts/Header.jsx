bf77588a4e4f2c7f032b6245958adfa69b884243
krishna <krishna.suryawanshi@e-zest.in>
2024-12-19 22:53:08 +0530

    dashboard changes

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,20 +1,46 @@
 import React from 'react';
+import './admin-dashboard.scss'
 const Header = () => {
   return (
     <>
-    <div className="contaioner-fluid">
-      <div className="row">
-        <div className="col-md-2 bg-primary" style={{height:'100vh'}}>
+      
+        <nav className='sidebar-wrapper'>
+          <div class="dropdown">
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="./profile-icon.webp" alt="" />
+              <span>Jonh Cena</span>
+            </a>
 
-        </div>
-        <div className="col-md-10 bg-danger p-0">
-          <div className="navbar col-12 bg-dark pt-5">
-
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
           </div>
-        </div>
-      </div>
-    </div>
-   
+
+          <div className='navbar-wrappper'>
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#"><i className="bi bi-house"></i> home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><i class="bi bi-person"></i> Profile</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><i class="bi bi-list-task"></i> Task</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><i class="bi bi-calculator"></i> Calculator</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><i class="bi bi-inbox"></i> Inbox</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        
+      
     </>
 
   );
--- src/pages/non-subcriber/Dashboard.jsx
+++ src/pages/non-subcriber/Dashboard.jsx
@@ -1,7 +1,120 @@
-import React from 'react';
+import React from "react";
 
 const Dashboard = () => {
-  return <h2>Dashboard</h2>;
+  return (
+    <div className="container-fluid p-0">
+      <div className="dashboard-main-content p-4">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-6">
+              <div class="card flex-fill">
+								<div class="card-body py-4">
+									<div class="d-flex align-items-start">
+										<div class="flex-grow-1">
+											<h3 class="mb-2">$ 24.300</h3>
+											<p class="mb-2">Total Earnings</p>
+											<div class="mb-0">
+												<span class="badge badge-subtle-success me-2"> +5.35% </span>
+												<span class="text-muted">Since last week</span>
+											</div>
+										</div>
+										<div class="d-inline-block ms-3">
+											<div class="stat">
+												<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" data-lucide="dollar-sign" class="lucide lucide-dollar-sign align-middle text-success"><line x1="12" x2="12" y1="2" y2="22"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+            </div>
+
+            <div className="col-md-6">
+              <div class="card flex-fill">
+								<div class="card-body py-4">
+									<div class="d-flex align-items-start">
+										<div class="flex-grow-1">
+											<h3 class="mb-2">$ 24.300</h3>
+											<p class="mb-2">Total Earnings</p>
+											<div class="mb-0">
+												<span class="badge badge-subtle-success me-2"> +5.35% </span>
+												<span class="text-muted">Since last week</span>
+											</div>
+										</div>
+										<div class="d-inline-block ms-3">
+											<div class="stat">
+												<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" data-lucide="dollar-sign" class="lucide lucide-dollar-sign align-middle text-success"><line x1="12" x2="12" y1="2" y2="22"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <div className="card">
+                <div className="card-body">
+                  <h2>Latest Projects</h2>
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">Heading</th>
+                          <th scope="col">Heading</th>
+                          <th scope="col">Heading</th>
+                          <th scope="col">Heading</th>
+                          <th scope="col">Heading</th>
+                          <th scope="col">Heading</th>
+                          <th scope="col">Heading</th>
+                          <th scope="col">Heading</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th scope="row">1</th>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">2</th>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">3</th>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                          <td>Cell</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
